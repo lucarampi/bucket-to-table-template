c786d03a00473098ed2052f35ecfc904439875e6
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-11-24 02:26:56 -0300

    supabase key fix

--- service/supabase.ts
+++ service/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL! || 'https://.com ',
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! || '')
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! || '.')
 
 export default supabase
